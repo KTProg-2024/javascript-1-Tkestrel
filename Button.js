b8c27a130921d60ece69006739e7c8985e5bf6aa
Toby Hansen <>
2021-05-27 19:39:43 +0000

    finished assignment

--- Button.js
+++ Button.js
@@ -3,17 +3,27 @@ function cat()
 	document.getElementById("animal").innerHTML="cat";
 }
 
-function green()
-{
-	document.getElementById("color").style.color="green";
-}
-
 function dog()
 {
 	document.getElementById("animal").innerHTML="dog";
 }
 
+function bunny()
+{
+	document.getElementById("animal").innerHTML="bunny";
+}
+
+function green()
+{
+	document.getElementById("color").style.color="green";
+}
+
 function red()
 {
 	document.getElementById("color").style.color="red";
+}
+
+function orange()
+{
+	document.getElementById("color").style.color="orange";
 }
